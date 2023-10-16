9ef49df21fa54888c29594ab20db387dd4e1cccc
Parker <parkerharmon194@gmail.com>
2023-10-16 01:24:21 -0400

    Added character model and animations

--- 2D game/characters/player.gd
+++ 2D game/characters/player.gd
@@ -1,20 +1,33 @@
+extends CharacterBody2D
 # main player character
 
-extends CharacterBody2D
+@export var Speed: int = 50
+@export var Friction: float = 0.15
+@export var Acceleration: int = 40
 
 @export var move_speed : float = 100
 
+
+## PLAYER MOVEMENT
 func _physics_process(_delta):  # underscore represents unused parameter
+	var input_vector = Vector2.ZERO
 	# get input direction
-	var input_direction = Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
+	
+	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	
 	
 	# update velocity
-	velocity = input_direction * move_speed
+	velocity = input_vector.normalized() * move_speed
+	if velocity == Vector2.ZERO:  # If character isn't moving, aka no input we will use idle position
+		$AnimationTree.get("parameters/playback").travel("Idle")  
+	else:  # else we will update the animation to run in the given velocity
+		
+		$AnimationTree.get("parameters/playback").travel("Run")
+		$AnimationTree.set("parameters/Idle/blend_position", velocity)
+		$AnimationTree.set("parameters/Run/blend_position", velocity)
+		move_and_slide()
+		
 	
 	
-	# move and slide function uses velocity of character body to move character on map
-	move_and_slide()
 	
